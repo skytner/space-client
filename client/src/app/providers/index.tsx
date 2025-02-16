b9e5ab69faf6b5b510d3bb8f8b3a2d68565318f6
skytner <jolyskytner@gmail.com>
2025-02-16 23:26:03 +0500

    feat: i18n

--- client/src/app/providers/index.tsx
+++ client/src/app/providers/index.tsx
@@ -1,10 +1,14 @@
 import { FC, JSX } from 'react'
 import { BrowserRouter as BrowserRouterProvider } from 'react-router-dom'
+import { I18nextProvider } from 'react-i18next'
 
 import { RouteProvider } from './router'
+import i18n from '@/shared/i18n'
 
 export const WithProviders: FC = (): JSX.Element => (
   <BrowserRouterProvider>
-    <RouteProvider />
+    <I18nextProvider i18n={i18n}>
+      <RouteProvider />
+    </I18nextProvider>
   </BrowserRouterProvider>
 )
--- client/src/pages/example/page.tsx
+++ client/src/pages/example/page.tsx
@@ -1,7 +1,13 @@
-const ExamplePage = () => (
-    <div>asd
-        asdasdasdasd
+import { useTranslation } from 'react-i18next'
+
+const ExamplePage = () => {
+  const { t, i18n } = useTranslation()
+  return (
+    <div>
+      {t('exampleee')}
+      <button onClick={() => i18n.changeLanguage('ru')}>change lang</button>
     </div>
-)
+  )
+}
 
 export default ExamplePage
