a7be71995ebd21e37b68a3d81689af231883ca23
skytner <jolyskytner@gmail.com>
2025-02-16 19:43:16 +0500

    chore: initialize monorepo structure for client and server

---
+++ client/src/app/index.tsx
@@ -0,0 +1,8 @@
+import { Fragment } from "react/jsx-runtime";
+import "./index.scss";
+
+function Application() {
+  return <Fragment></Fragment>;
+}
+
+export default Application;
---
+++ client/src/entities/example/store.ts
@@ -0,0 +1,3 @@
+import { createStore } from 'effector'
+
+export const $example = createStore(0)
